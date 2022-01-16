d53992e4cc0a73bc431613bea0164b21e16c1548
adriansteffan <adrian.steffan@hotmail.de>
2022-01-16 21:12:04 +0100

    Add regex cleanup

--- convert.py
+++ convert.py
@@ -1,10 +1,23 @@
 import sys
 import os
+import re
 from pathlib import Path
 
 
-def process_directory(input_dir, output_dir, visibility_dict):
+def cleanup_content(content, custom_replaces):
+    if custom_replaces:
+        if "Zotero Links: [Local]" in content:
+            content = re.sub(r"(?<=^- ).*(?=$)", "Info:", content, 1, re.MULTILINE)  # BetterBibtex Ref
+            content = re.sub(r"^ *- Zotero Links: \[Local\].*$", "", content, 0, re.MULTILINE)  # Zotero Links
 
+    content = re.sub(r'(?<=\[\[)[^[|]*\|(?=[^]]*\]\])', '', content)  # aliases
+    content = re.sub(r"&(?=[^\]\[]*\]\])", "and", content)  # & to and
+    return content
+
+
+def process_directory(input_dir, output_dir, visibility_dict, custom_replaces):
+
+    #print(input_dir)
     # private by default
     directory_public = False
 
@@ -26,7 +39,7 @@ def process_directory(input_dir, output_dir, visibility_dict):
         curr_file_path = os.path.join(input_dir, file)
 
         if os.path.isdir(curr_file_path):
-            process_directory(curr_file_path, output_dir, visibility_dict)
+            process_directory(curr_file_path, output_dir, visibility_dict, custom_replaces)
             continue
 
         if not file.endswith(".md") or file.startswith("."):
@@ -44,24 +57,26 @@ def process_directory(input_dir, output_dir, visibility_dict):
                 elif not directory_public:
                     continue
 
-                output = f'---\ntitle: "{title_clean}"\n{content[4:]}'
+                output = f'---\ntitle: "{title_clean}"\n{cleanup_content(content[4:], custom_replaces)}'
             else:
                 if not directory_public:
                     continue
 
-                output = f'---\ntitle: "{title_clean}"\n---\n{content}'
+                output = f'---\ntitle: "{title_clean}"\n---\n{cleanup_content(content, custom_replaces)}'
 
         with open(os.path.join(output_dir, file.replace("&", "and")), "w") as f:
             f.write(output)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    # an arbitrary third parameter applies my custom string replaces for my setup
+
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
         print("Invalid number of commandline parameters")
         exit(1)
 
-    input_dir = os.path.join(os.getcwd(), sys.argv[1])
-    output_dir = os.path.join(os.getcwd(), sys.argv[2])
+    input_dir = os.path.join(os.getcwd(), os.path.normpath(sys.argv[1]))
+    output_dir = os.path.join(os.getcwd(), os.path.normpath(sys.argv[2]))
 
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
@@ -70,4 +85,4 @@ if __name__ == '__main__':
             os.remove(os.path.join(output_dir, f))
 
     visibility_dict = dict()
-    process_directory(input_dir, output_dir, visibility_dict)
+    process_directory(input_dir, output_dir, visibility_dict, len(sys.argv) == 4)
